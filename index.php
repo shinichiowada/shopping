fb824a53981f4abaab110fd632663a3b0c1c7026
shinichi <0503baku@gmail.com>
2021-02-25 09:20:43 +0900

    一回目の修正

--- index.php
+++ index.php
@@ -9,57 +9,34 @@ foreach ($moneys as $money_num => $select_money) {
 }
 
 echo "番号:";
-$msg = fgets(STDIN);
+$msg = trim(fgets(STDIN));
 $charge = "円チャージしました。" . PHP_EOL;
 
 switch ($msg) {
     case 1:
-        $price = $moneys["1"];
-        echo $price . $charge;
-        break;
-
     case 2:
-        $price = $moneys["2"];
-        echo $price . $charge;
-        break;
-
     case 3:
-        $price = $moneys["3"];
+        $price = $moneys[$msg];
         echo $price . $charge;
         break;
 
     default:
-        break;
-}
+        echo "無効な番号です" . PHP_EOL . "処理を終了します。" . PHP_EOL . "買い物を終了します。" . PHP_EOL;
+        exit;
 
-if (isset($price)) {
-    $flag = "true";
-}
-
-if ($flag !== "true") {
-    echo "無効な番号です" . PHP_EOL . "処理を終了します。" . PHP_EOL . "買い物を終了します。" . PHP_EOL;
-    exit;
+        $flag = true;
 }
 
 do {
     echo "商品の価格を入力して下さい:";
-    $goods = fgets(STDIN);
+    $goods = trim(fgets(STDIN));
 
-    if (isset($num)) {
-        $num = $num - $goods;
-    } else {
-        $num = ($price - $goods);
+    if (isset($price)) {
+        $price = $price - $goods;
     }
 
-    if ($num < 0) {
-        $flag = "false";
-        break;
-    }
+    echo "残高は{$price}円です。" . PHP_EOL;
+} while ($price >= 0);
 
-    echo "残高は{$num}円です。" . PHP_EOL;
-} while ($num >= 0);
-
-if ($flag == "false") {
-    echo "チャージ金額を上回るため購入できません。" . PHP_EOL . "買い物を終了します。" . PHP_EOL;
-    exit;
-}
+echo "チャージ金額を上回るため購入できません。" . PHP_EOL . "買い物を終了します。" . PHP_EOL;
+exit;
